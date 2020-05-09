f8ab79640e8a5ef57795a3392d965fc49595ae48
sidneyramos <sidney.ramos95@gmail.com>
2020-05-09 18:12:35 +1000

    Omit prismic script

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,8 @@ module.exports = {
       resolve: "gatsby-source-prismic-graphql",
       options: {
         repositoryName: "notjustaboxevents", // (REQUIRED, replace with your own)
-        linkResolver: () => post => `/${post.uid}`
+        linkResolver: () => post => `/${post.uid}`,
+        omitPrismicScript: true
       }
     },
     {
