e9ce4574d9ddfa74a1e3bee03933549ed72c8616
sidneyramos <sidney.ramos95@gmail.com>
2020-05-17 00:50:34 +1000

    Fixed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,60 @@
 import React from "react";
 import Layout from "components/Layout";
+import colors from "styles/colors";
+import styled from "@emotion/styled";
+
+import { Heading, Grid, Text, Image, Flex, Box } from "@chakra-ui/core";
+import Link from "components/_ui/Link";
+
+const UnderlineBox = styled(Box)`
+  position: relative;
+
+  &:after {
+    position: absolute;
+    content: "";
+    height: 2px;
+    width: 25%;
+    background-color: ${colors.njabDarkPink};
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+  }
+`;
+
+const HomeLink = styled(Link)`
+  text-transform: uppercase;
+  border: 1px solid ${colors.njabMidPink};
+  padding: 10px 25px;
+  font-size: 0.8rem;
+  font-weight: 600;
+
+  &:hover {
+    text-decoration: initial;
+    background-color: ${colors.njabLightPink};
+  }
+`;
 
 const NotFoundPage = () => (
-    <Layout>
-        <h1>
-            NOT FOUND
-        </h1>
-        <p>
-            You just hit a route that doesn&#39;t exist... the sadness.
-        </p>
-    </Layout>
-)
+  <Layout>
+    <Flex
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      textAlign="center"
+      color={colors.njabDarkPink}
+      height="calc(100vh - 410px)"
+    >
+      <UnderlineBox>
+        <Heading as="h1" margin="0" fontSize="5rem">
+          404
+        </Heading>
+      </UnderlineBox>
+      <Text fontWeight="700" letterSpacing="3px">
+        OOPS! PAGE NOT FOUND
+      </Text>
+      <HomeLink to="/home">Go back to home</HomeLink>
+    </Flex>
+  </Layout>
+);
 
 export default NotFoundPage;
