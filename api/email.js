7cd255302bc35cb57aae5f8b03818e01a96f7417
sidneyramos <sidney.ramos95@gmail.com>
2020-06-18 00:53:50 +1000

    This is the real trial

--- api/email.js
+++ api/email.js
@@ -1,26 +1,21 @@
 require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
-// sgMail.setApiKey(process.env.GATSBY_SENDGRID);
+sgMail.setApiKey(process.env.GATSBY_SENDGRID);
 
 module.exports = (req, res) => {
   const { body } = req;
-  //   const msg = {
-  //     to: "team.r202creatives@gmail.com",
-  //     from: "test@notjustaboxevents.com",
-  //     subject: "Sending with Twilio SendGrid is Fun",
-  //     text: "and easy to do anywhere, even with Node.js",
-  //     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-  //   };
-  res
-    .status(200)
-    .send(
-      `${process.env.GATSBY_SENDGRID} ${
-        process.env.SENDGRID_API
-      }- Sent email: ${JSON.stringify(body)}`
-    );
-  //   sgMail.send(msg).then(() => {
-  //     res
-  //       .status(200)
-  //       .send(`${process.env.GATSBY_SENDGRID} - Sent email: ${body}`);
-  //   });
+  const msg = {
+    to: "team.r202creatives@gmail.com",
+    from: "test@notjustaboxevents.com",
+    subject: "Sending with Twilio SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail.send(msg).then(() => {
+    res
+      .status(200)
+      .send(
+        `${process.env.GATSBY_SENDGRID} - Sent email: ${JSON.stringify(body)}`
+      );
+  });
 };
