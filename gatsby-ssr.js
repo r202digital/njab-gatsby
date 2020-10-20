1a53c926d20a536e0ed2f893836ce69afd32ccf2
sidneyramos <sidney.ramos95@gmail.com>
2020-10-21 02:28:51 +1100

    Meta fixes

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,19 @@
  */
 
 // You can delete this file if you're not using it
+
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  replaceHeadComponents,
+}) => {
+  const headComponents = getHeadComponents();
+  headComponents.sort((x, y) => {
+    if (x.type === "meta") {
+      return -1;
+    } else if (y.type === "meta") {
+      return 1;
+    }
+    return 0;
+  });
+  replaceHeadComponents(headComponents);
+};
